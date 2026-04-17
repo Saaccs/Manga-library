24cc83efde554e8d10d2fb54c6781b911a710cc1
IsaacCj <isaaccj763@gmail.com>
2026-04-17 02:18:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+let data = [];
 
+fetch("data.json")
+  .then(res => res.json())
+  .then(json => {
+    data = json;
+    render(data);
+  });
+
+function render(list) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+
+  list.forEach(m => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${m.cover}" width="100">
+        <h3>${m.title}</h3>
+        <a href="${m.url}" target="_blank">Ver</a>
+        <p>${m.status}</p>
+      </div>
+    `;
+  });
+}
+
+document.getElementById("search").addEventListener("input", e => {
+  const value = e.target.value.toLowerCase();
+  const filtered = data.filter(m =>
+    m.title.toLowerCase().includes(value)
+  );
+  render(filtered);
+});
